44b4018962bedcf0ebdbf5f76fe63ecf8b56ca98
masterful-array-3850 <crgammage@gmail.com>
2020-04-13 14:29:46 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,16 +13,18 @@ const codes = [
 
   function init() {
     var index = 0
-    function onKeyDownHandler(e) {
-      const key = parseInt(key.detail || key.which)
-      console.log('key', key)
-      if (key === codes[index]) {
-        index++
-        if (index === codes.length) {
-          alert("YOU DID IT!")
-          index = 0
-        } else {
-        index = 0
-      }
-    } document.body.addEventListener('keydown', onKeyDownHandler());
+    // function onKeyDownHandler(e) {
+    //   const key = parseInt(key.detail || key.which)
+    //   console.log('key', key)
+    //   if (key === codes[index]) {
+    //     index++
+    //     if (index === codes.length) {
+    //       alert("YOU DID IT!")
+    //       index = 0
+    //     } else {
+    //     index = 0
+    //   }
+  } document.body.addEventListener('keydown', function(e){
+    console.log(e)
+  });
   }	}
